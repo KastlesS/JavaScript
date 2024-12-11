14cf2389b15bbc67b97576d4f03a7e53f0e1ef6b
Ivan Castillo <ivacassim@alu.edu.gva.es>
2024-12-11 09:40:31 +0100

    frizo la bola

---
+++ Unidad4/ActividadObligatoria/validacion.js
@@ -0,0 +1,7 @@
+/* Ejercicio 2 */
+function validarNombreApp(nombre, apellidos){
+    let expresion;
+}
+
+let nombre = document.getElementById("nombre").value;
+let apellidos = document.getElementById("apellidos").value;
---
+++ Unidad4/Formularios para clase/f4.js
@@ -0,0 +1,5 @@
+
+
+        document.getElementById("formulario").submit();
+ 
+
